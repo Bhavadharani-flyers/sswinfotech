41404a1386b3be8e389b6a0cfb43c8b079f6cc03
bhavadharani-flyers <bhavadharani.palanivelu@flyerssoft.com>
2023-11-04 11:55:23 +0530

    Get started button removed blog link added

--- src/Pages/Coins/index.tsx
+++ src/Pages/Coins/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { cryptobg, cryptobg1 } from "../../assets";
+import { cryptobg, cryptobg1 } from "../../assets/images";
 
 export const Coins = () => {
   return (
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,13 +2,17 @@ import { Link } from "react-router-dom";
 import { Tab } from "../../Components/Button/Tab";
 import Team from "./Team";
 import Aboutus from "./Aboutus";
+import { Blog } from "../../assets/icons";
 
 export const Home = () => {
   return (
     <div className=" text-white ">
-      <div className="">
+      <div className="flex flex-row items-center justify-end gap-2 pr-10 pt-5 ">
         {/* <img src={Bgimg} /> */}
-        <div className="flex flex-row"></div>
+        <p className="text-xl font-bold">Our Blogs</p>
+        <Link to="blog" className="">
+          <Blog />
+        </Link>
       </div>
       <section className="flex flex-row gap-5 items-center pl-16">
         <div className="flex flex-col pl-5">
@@ -18,13 +22,11 @@ export const Home = () => {
           <p className="text-left mr-[50%]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
             dolorem beatae nesciunt tempora. Possimus, libero deserunt?
-            Obcaecati ipsum a officiis sequi eum numquam quibusdam, dignissimos
-            harum in, vitae vel molestiae.
           </p>
           <div className="pt-2">
-            <button className="box-border h-10 w-24 bg-[#22d3ee] rounded-lg ">
+            {/* <button className="box-border h-10 w-24 bg-[#22d3ee] rounded-lg ">
               Get Started
-            </button>
+            </button> */}
           </div>
 
           {/* <div className="pt-8 gap-6 flex flex-row ">
